926dd47e8b1a608f64ca3b98b3c2aca15bb4481b
Sumit kumar Ranjan <136005751+sumitx99@users.noreply.github.com>
2025-12-07 03:20:46 +0530

    Revert "changes"

--- app/page.tsx
+++ app/page.tsx
@@ -382,10 +382,9 @@ if (response.ok) {
           }}
         ></motion.div>
       </div>
-      {/* Matrix Rain Effect - Hidden on mobile to stop lag */}
-<div className="hidden md:block">
-  <MatrixRain />
-</div>
+      {/* Matrix Rain Effect */}
+      <MatrixRain />
+
 
       {/* Removed the CustomCursor component */}
 
